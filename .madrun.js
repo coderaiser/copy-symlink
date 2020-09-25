fef40103f8211995a2d378d8a5ab3b567c305be3
coderaiser <coderaiser@cloudcmd.io>
2020-09-25 17:51:43 +0300

    feature(copy-symlink) drop support of node < 10

---
+++ .madrun.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const {run} = require('madrun');
+
+module.exports = {
+    'test': () => 'tape test/*.js',
+    'fix:lint': () => run('lint', '--fix'),
+    'lint': () => 'putout lib test .madrun.js',
+    'watch:test': () => run('watcher', '"npm test"'),
+    'watcher': () => 'nodemon -w lib -w test -x',
+    'coverage': () => 'nyc npm test',
+    'report': () => 'nyc report --reporter=text-lcov | coveralls',
+};
+
--- test/copy-symlink.js
+++ test/copy-symlink.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs');
-const test = require('tape');
+const test = require('supertape');
 const tryToCatch = require('try-to-catch');
 const {reRequire}  = require('mock-require');
 
@@ -14,26 +14,26 @@ test('copy-symlink', async (t) => {
 });
 
 test('copy-symlink: realpath', async (t) => {
-    const {realpath} = fs;
-    fs.realpath = (a, fn) => fn(null, '/hello');
+    const {realpath} = fs.promises;
+    fs.promises.realpath = () => '/hello';
     
     const copySymlink = reRequire('..');
     const [e] = await tryToCatch(copySymlink, '/', '/abc');
     
-    fs.realpath = realpath;
+    fs.promises.realpath = realpath;
     
     t.equal(e.path, '/hello', 'should equal');
     t.end();
 });
 
 test('copy-symlink: symlink', async (t) => {
-    const {symlink} = fs;
-    fs.symlink = (a, b, fn) => fn(Error('hello'));
+    const {symlink} = fs.promises;
+    fs.promises.symlink = () => {throw Error('hello');};
     
     const copySymlink = reRequire('..');
     const [e] = await tryToCatch(copySymlink, '/', '/abc');
     
-    fs.symlink = symlink;
+    fs.promises.symlink = symlink;
     
     t.equal(e.message, 'hello', 'should equal');
     t.end();
